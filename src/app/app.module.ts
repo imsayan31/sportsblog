7cc83d8f44d7a4e401b419d7750c209656dfcb8d
Sayan Dey <sayantadeytraining@gmail.com>
2020-11-26 22:42:30 +0530

    test comp added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TestSportsComponent } from './test-sports/test-sports.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TestSportsComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/test-sports/test-sports.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-test-sports',
+  templateUrl: './test-sports.component.html',
+  styleUrls: ['./test-sports.component.css']
+})
+export class TestSportsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
